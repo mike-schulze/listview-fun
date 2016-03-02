1144c05c0260b78f5a4857cd3e29f75d50b1a0df
Mike Schulze <mike.schulze@garmin.com>
2016-03-01 18:39:16 -0700

    Missed two files in last commit.

---
+++ ListViewFun/Scenarios/HardCodedSizesWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ListViewFun.Scenarios
+{
+    /// <summary>
+    /// Demonstrates that hard-coding the height of items displayed in a list-view has a huge performance impact
+    /// </summary>
+    public partial class HardCodedSizesWindow : Window
+    {
+        public HardCodedSizesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
